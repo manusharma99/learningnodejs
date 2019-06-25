c4d36b48936d32df5bd9a1a7723f99314a5311be
unknown <manusharma8826@gmail.com>
2019-06-25 23:46:35 +0530

    updated

--- app.js
+++ app.js
@@ -100,3 +100,26 @@
 //         }
 //     })
 // });
+
+/*######creating server######*/
+// var http = require('http');
+
+// var server = http.createServer(function(req,res){
+//     console.log('request was made: '+req.url);
+//     res.writeHead(200,{'content-Type':'text/plain'})
+//     res.end('hey manu');
+// });
+
+// server.listen(3000,'127.0.0.1');
+// console.log('yo imma listening');
+
+/*######readable stream#####*/
+// var http = require('http');
+// var fs = require('fs');
+
+// var mystreamread = fs.createReadStream(__dirname + '/readme.txt','utf8');
+
+// mystreamread.on('data',function(chunk){
+// console.log('new chunk recieved: ');
+// console.log(chunk);
+// });
